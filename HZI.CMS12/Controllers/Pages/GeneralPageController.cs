0c9dfbf8c22f7d3b838b20635fb42a6b19e1198c
Nathan Hase <nhase@horizontal.com>
2025-04-02 08:35:34 -0500

    working with block data

--- HZI.CMS12/Controllers/Pages/GeneralPageController.cs
+++ HZI.CMS12/Controllers/Pages/GeneralPageController.cs
@@ -1,12 +1,48 @@
-﻿using HZI.CMS12.Models.Pages;
+﻿using HZI.CMS12.Models.Blocks;
+using HZI.CMS12.Models.Pages;
 using Microsoft.AspNetCore.Mvc;
 
 namespace HZI.CMS12.Controllers.Pages
 {
     public class GeneralPageController : BasePageController<GeneralPage>
     {
+        public readonly IContentLoader contentLoader;
+
+        public GeneralPageController(IContentLoader contentLoader)
+        {
+            this.contentLoader = contentLoader;
+        }
+
         public IActionResult Index(GeneralPage currentContent)
         {
+            if (currentContent.BottomContentArea != null)
+            {
+                var blockReference = currentContent.BottomContentArea.FilteredItems;
+                foreach (var reference in blockReference)
+                {
+                    var block = contentLoader.Get<BlockData>(reference.ContentLink);
+
+                    if (block is SectionMediaBlock sectionMedia)
+                    {
+                    }
+                    else if (block is LinkGridBlock linkGrid)
+                    {
+                    }
+
+                    /*
+                    var blockType = block.GetOriginalType();
+
+                    if(blockType == typeof(SectionMediaBlock))
+                    {
+
+                    }else if(blockType == typeof(LinkGridBlock))
+                    {
+
+                    }
+                    */
+                }
+
+            }
             return PageView(currentContent);
         }
     }
